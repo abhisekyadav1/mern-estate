15935f08b5b5861ee8bcdb2623e6d919d1b4249e
abhisekyadav1 <ak5656747@gmail.com>
2024-06-02 16:36:44 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,20 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000',
+//         secure: false,
+//       },
+//     },
+//   },
+
+//   plugins: [react()],
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
@@ -12,5 +29,10 @@ export default defineConfig({
     },
   },
 
+  build: {
+    outDir: 'public', // Set the output directory to 'public'
+  },
+
   plugins: [react()],
 });
+
